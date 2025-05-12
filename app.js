ad140f5c10f0492039367a17126fa02fc37708d0
smerdyakov-moi <pragyankhati45@gmail.com>
2025-05-12 15:13:41 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,111 @@
+const express = require ('express')
+const app = express()
+const userModel = require ("./models/user")
+const postModel = require ("./models/post")
+const bcrypt = require ('bcrypt')
+const jwt = require ('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const post = require('./models/post')
+
+app.set ('view engine','ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get ('/profile',isLoggedin,async (req,res)=>{
+    let user = await userModel.findOne({_id:req.user.userid}).populate("posts")
+    res.render('profile',{user})
+})
+
+app.get ('/like/:id',isLoggedin,async (req,res)=>{
+    let post = await postModel.findOne({_id:req.params.id}).populate("user")
+    
+    if (post.likes.indexOf(req.user.userid) === -1) {post.likes.push(req.user.userid)}
+    else {post.likes.splice(post.likes.indexOf(req.user.userid),1)}
+    
+    
+    await post.save()
+    res.redirect('/profile')
+})
+
+app.get ('/login', (req,res)=>{
+    res.render('login')
+})
+
+app.post('/register',async (req,res)=>{
+    let {email,password,username,name,age} = req.body
+    let user = await userModel.findOne({email})
+    if (user) return res.status(500).send('User already registered')
+    
+     let hash_pw = await bcrypt.hash(password,10)
+     let createdUser = await userModel.create({
+        email,password:hash_pw,username,name,age
+     })
+     let token = jwt.sign({email:email, userid: createdUser._id},'Secret')
+     res.cookie('token',token)
+     res.send(createdUser)
+})
+
+app.post('/login', async (req,res)=>{
+    let {email,password} = req.body
+    let user = await userModel.findOne({email})
+    if (!user) return res.status(500).send('No user with such email!')
+
+    bcrypt.compare(password, user.password, (err,result)=>{
+        if (result) {
+            let token = jwt.sign({email:email, userid: user._id},'Secret')
+            res.cookie('token',token)
+            res.redirect('/profile')
+        }
+        else {res.redirect('/login')}
+    })
+})
+
+app.post('/createPost', isLoggedin, async(req,res)=>{
+    let {caption,body} = req.body
+    let user = await userModel.findOne({_id:req.user.userid})
+    let post = await postModel.create({
+        user: user._id,
+        caption,body,
+
+    })
+    user.posts.push(post._id)
+    await user.save()
+    res.redirect('/profile')
+})
+
+app.get('/editProfile', isLoggedin, async(req,res)=>{
+    let user = await userModel.findOne({_id:req.user.userid})
+    console.log(user)
+    res.render('editprofile',{user})
+})
+
+app.post('/updateProfile', isLoggedin, async(req,res)=>{
+    let {username,name} = req.body
+    console.log({username,name})
+    await userModel.findOneAndUpdate({_id:req.user.userid},{username,name},{new:true})
+    res.redirect('/profile')
+})
+
+app.get('/logout', (req,res)=>{
+    res.cookie('token',"")
+    res.redirect('/login')
+})
+
+
+function isLoggedin(req,res,next){
+    if (req.cookies.token === "") res.redirect("/login")
+    else{
+        let data = jwt.verify( req.cookies.token,"Secret")
+        req.user = data
+        next()
+}
+
+}
+app.listen('3000',()=>{
+    console.log('Listening...')
+})
---
+++ models/post.js
@@ -0,0 +1,19 @@
+const mongoose = require ('mongoose')
+
+const postSchema = mongoose.Schema ({
+    user:{type: mongoose.Schema.Types.ObjectId, ref:'user'},
+    data:{
+        type: Date,
+        default: Date.now
+    },
+    caption: String,
+    body:String,
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId, ref: 'user'
+        }
+    ]
+})
+
+
+module.exports = mongoose.model ('post', postSchema)
